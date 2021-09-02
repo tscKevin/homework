ff7638cfd0efd4915c98063ce68ec6983ca7a7f3
Kevin <xo3374ox@gmail.com>
2021-09-03 01:12:34 +0800

    part of Kotlin finish.

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Tester {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Please enter number of tickets:");
+        System.out.print("Please enter number of tickets: ");
         int numberTicket = scanner.nextInt();
         System.out.print("How many round-trip tickets: ");
         int roundTripNumberTickets = scanner.nextInt();
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -1,4 +1,20 @@
 package com.train
 
-class TesterKotlin {
+import java.util.*
+
+class TicketKt (val numberTicket: Int, val numberRoundTrip: Int){
+    fun compute(){
+        print(((numberTicket-numberRoundTrip) *1000+numberRoundTrip*2000*0.9).toInt())
+    }
+}
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    print("Please enter number of tickets:")
+    var numberTicket = scanner.nextInt()
+    print("How many round-trip tickets: ")
+    var roundTripNumberTickets = scanner.nextInt()
+
+    val ticket = TicketKt(numberTicket,roundTripNumberTickets)
+    ticket.compute()
 }
